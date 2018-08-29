f925b533722e55f58215a89814892abb428e67ca
DONGYU <zhangdongyu963@outlook.com>
2018-08-29 15:12:53 +0800

    修改了打开文件没有close的情况 增加了注释

--- pics_joint.py
+++ pics_joint.py
@@ -9,10 +9,10 @@ def cover_pic(name):
     :return: 
     """
     cover_path = 'pics/' + str(name) + '.png'
-    cover = Image.open(cover_path)
-    cover_resize = cover.resize((75,75))
-    background = Image.open('pics/link.png')
-    bg = background.copy()
+    with Image.open(cover_path) as cover:
+        cover_resize = cover.resize((75,75))
+    with Image.open('pics/link.png') as background:
+        bg = background.copy()
     bg.paste(cover_resize,(10,22))
     # bg.show()
     bg_path = 'pics/' + str(name) + '.png'
@@ -56,30 +56,30 @@ def paste_title(name):
     """
     # count = 0
     bg_path = 'pics/' + name + '.png'
-    background = Image.open(bg_path)
-    title = ImageDraw.Draw(background)
-    Ping = ImageFont.truetype('font/Ping.ttf', 26)
-    SFPD = ImageFont.truetype('font/SFPD.otf',26)
-    title_color = "#000000"
-    line_1,line_2 = split_title(name)
+    with Image.open(bg_path) as background:
+        title = ImageDraw.Draw(background)
+        Ping = ImageFont.truetype('font/Ping.ttf', 26)
+        SFPD = ImageFont.truetype('font/SFPD.otf',26)
+        title_color = "#000000"
+        line_1,line_2 = split_title(name)
 
-    if line_2:
-        # cursor = 97
-        # for char in line_1:
-        #     uni_code = ord(char)
-        #     if (uni_code >= 33 and uni_code <= 64) or (uni_code >= 91 and uni_code <= 126):
-        #         title.text((cursor,32),char,font=SFPD,fill=title_color)
-        #         cursor += 8    # 半角占18个单位
-        #         print(char,cursor)
-        #     elif (uni_code >= 19968 and uni_code <= 40869) or (uni_code>=65 and uni_code<=90):
-        #         title.text((cursor,27),char,font=Ping,fill=title_color)
-        #         cursor += 25
-        #         print(char,cursor)
-        title.text((97,27),line_1,font=Ping, fill=title_color)
-        title.text((97,56),line_2,font=Ping,fill=title_color)
-    else:
-        title.text((97,48),name,font=Ping,fill=title_color)
-    background.save(bg_path)
+        if line_2:
+            # cursor = 97
+            # for char in line_1:
+            #     uni_code = ord(char)
+            #     if (uni_code >= 33 and uni_code <= 64) or (uni_code >= 91 and uni_code <= 126):
+            #         title.text((cursor,32),char,font=SFPD,fill=title_color)
+            #         cursor += 8    # 半角占18个单位
+            #         print(char,cursor)
+            #     elif (uni_code >= 19968 and uni_code <= 40869) or (uni_code>=65 and uni_code<=90):
+            #         title.text((cursor,27),char,font=Ping,fill=title_color)
+            #         cursor += 25
+            #         print(char,cursor)
+            title.text((97,27),line_1,font=Ping, fill=title_color)
+            title.text((97,56),line_2,font=Ping,fill=title_color)
+        else:
+            title.text((97,48),name,font=Ping,fill=title_color)
+            background.save(bg_path)
 
 
 def userhead(usrn):
@@ -88,11 +88,11 @@ def userhead(usrn):
     :param usrn: 用户名
     :return: 
     """
-    background = Image.open('pics/head_null.png')
-    bg = background.copy()
+    with Image.open('pics/head_null.png') as background:
+        bg = background.copy()
     head_path = 'pics/portrail/' + str(usrn) + '.jpeg'
-    head = Image.open(head_path)
-    head_resize = head.resize((80,80))
+    with Image.open(head_path) as head:
+        head_resize = head.resize((80,80))
     bg.paste(head_resize,(24,20))
     bg.save('pics/head_mdf.png')
 
@@ -103,8 +103,8 @@ def username(usrn):
     :param username: 用户名
     :return: 
     """
-    back_ground = Image.open('pics/name.png')
-    bg = back_ground.copy()
+    with Image.open('pics/name.png') as back_ground:
+        bg = back_ground.copy()
     name = ImageDraw.Draw(bg)
     Ping = ImageFont.truetype('font/Ping_Bold.ttf',30)
     name_color = "#5B6A92"
@@ -119,10 +119,10 @@ def likes(num):
     :return: 
     """
     row = int(ceil(num / 7))
-    print("total rows:",row)
+    # print("total rows:",row)
     bg_path = 'pics/likes/likes' + str(row) + '.png'
-    background = Image.open(bg_path)
-    bg = background.copy()
+    with Image.open(bg_path) as background:
+        bg = background.copy()
     if num > 49:
         count = 49
     else:
@@ -132,18 +132,18 @@ def likes(num):
     for r in range(row-1):
         for i in range(index,index+7):
             prtl_path = 'pics/portrail/' + str(i) + '.jpg'
-            prtl = Image.open(prtl_path)
             xcoor = 20 + 68 + (70+10) * (i-index)
-            bg.paste(prtl,(xcoor,28+ r*80))
-        print("row:",r)
+            with Image.open(prtl_path) as prtl:
+              bg.paste(prtl,(xcoor,28+ r*80))
+        # print("row:",r)
         index += 7
-        print('index:',index)
+        # print('index:',index)
     for i in range(index,count+1):
-        print(i)
+        # print(i)
         prtl_path = 'pics/portrail/' + str(i) + '.jpg'
-        prtl = Image.open(prtl_path)
         xcoor = 20 + 68 + (70 + 10) * (i - index)
-        bg.paste(prtl, (xcoor, 28 + (row-1) * 80))
+        with Image.open(prtl_path) as prtl:
+            bg.paste(prtl, (xcoor, 28 + (row-1) * 80))
     bg.save('pics/likes_mdf.png')
 
 
@@ -152,37 +152,37 @@ def combine_pics(ttl):
     将各部分图片进行拼接
     :return: 
     """
-    background = Image.open('pics/null.png')
-    bg = background.copy()
+    with Image.open('pics/null.png') as background:
+        bg = background.copy()
 
     # 放置顶部状态栏
-    top = Image.open('pics/TOP.png')
-    bg.paste(top,(0,0))
+    with Image.open('pics/TOP.png') as top:
+        bg.paste(top,(0,0))
 
     # 放置头像
-    head = Image.open('pics/head_mdf.png')
-    bg.paste(head,(0,128))
+    with Image.open('pics/head_mdf.png') as head:
+        bg.paste(head,(0,128))
 
     # 放置昵称
-    name = Image.open('pics/name_mdf.png')
-    bg.paste(name,(124,128))
+    with Image.open('pics/name_mdf.png')as name:
+        bg.paste(name,(124,128))
 
     # 放置链接
     link_path = 'pics/' + str(ttl) + '.png'
-    link = Image.open(link_path)
-    bg.paste(link,(124,181))
+    with Image.open(link_path) as link:
+        bg.paste(link,(124,181))
 
     # 放置时间
-    time = Image.open('pics/time.png')
-    bg.paste(time,(124,299))
+    with Image.open('pics/time.png') as time:
+        bg.paste(time,(124,299))
 
     # 放置点赞
-    like = Image.open('pics/likes_mdf.png')
-    bg.paste(like,(0,353))
+    with Image.open('pics/likes_mdf.png') as like:
+        bg.paste(like,(0,353))
 
     # 放置底部评论栏
-    btm = Image.open('pics/BTM.png')
-    bg.paste(btm,(0,1248))
+    with Image.open('pics/BTM.png') as btm:
+        bg.paste(btm,(0,1248))
 
     bg = bg.convert('RGB')
     bg.save('final.jpg')
